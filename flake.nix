1e1a596f425d122b42832935615211594f7a99fa
Ryan Taylor Long <ryan@rtlong.com>
2024-07-31 11:47:38 -0400

    Add flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }@inputs:
+    rec {
+      sdImageSystem = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [ ./sd-image.nix ];
+      };
+
+      sdImage = sdImageSystem.config.system.build.sdImage;
+    };
+
+}
--- sd-image.nix
+++ sd-image.nix
@@ -1,18 +1,26 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
   imports = [
     (modulesPath + "/profiles/base.nix")
     (modulesPath + "/profiles/installation-device.nix")
-    (modulesPath + "/installer/cd-dvd/sd-image.nix")
+    (modulesPath + "/installer/sd-card/sd-image.nix")
     ./default.nix
   ];
   sdImage = {
-    populateFirmwareCommands = let
-      configTxt = pkgs.writeText "README" ''
-        Nothing to see here. This empty partition is here because I don't know how to turn its creation off.
-      '';
-      in ''
+    populateFirmwareCommands =
+      let
+        configTxt = pkgs.writeText "README" ''
+          Nothing to see here. This empty partition is here because I don't know how to turn its creation off.
+        '';
+      in
+      ''
         cp ${configTxt} firmware/README
       '';
     populateRootCommands = ''
